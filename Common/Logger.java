f42e0ece0ef36c25bbe1f82fd4d0a4893f9ac0fb
arl7816 <arl7816@g.rit.edu>
2026-01-29 17:53:51 -0500

    implements the logging tool

--- Common/Logger.java
+++ Common/Logger.java
@@ -1,4 +1,36 @@
 package Common;
 
 public class Logger {
+    private static boolean debugOn;
+
+    public static boolean initDebug(String[] args){
+        for (String str: args){
+            if (str.equals("--debug")) {
+                Logger.debugOn = true;
+                break;
+            }
+        }
+
+        return Logger.debugOn;
+    }
+
+    public static void log(String str){
+        if (Logger.debugOn){
+            System.out.println(str);
+        }
+
+    }
+
+    public static void logBase(String str){
+        if (Logger.debugOn){
+            System.out.print(str);
+        }
+
+    }
+
+    public static void main(String[] args){
+        Logger.initDebug(args);
+        Logger.log("Hello World");
+
+    }
 }
