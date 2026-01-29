9759a74e8702bb219589c152ceaf98b23902fa34
Logan <lpm5781@g.rit.edu>
2026-01-29 10:32:52 -0500

    turned catalog and storage manager to singletons

--- Catalog/Catalog.java
+++ Catalog/Catalog.java
@@ -1,4 +1,16 @@
 package Catalog;
 
 public class Catalog {
+    private static Catalog catalog;
+    public static void createCatalog() {
+        catalog = new Catalog();
+    }
+    private Catalog() {
+
+    }
+
+    // public static int getValue(...){
+    //     return catalog.method();
+    // }
+
 }
--- StorageManager/StorageManager.java
+++ StorageManager/StorageManager.java
@@ -1,4 +1,18 @@
 package StorageManager;
 
 public class StorageManager {
+    private static StorageManager storageManager;
+    public static void createStorageManager() {
+
+        storageManager = new StorageManager();
+    }
+    private StorageManager() {
+
+    }
+
+    // public static int getValue(...){
+    //     return storageManager.method();
+    // }
+
+
 }
