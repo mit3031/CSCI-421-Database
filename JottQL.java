c4c5cb5967d941a18b27d0f39c52fcb4ed6ec637
Antonio <acb9430@rit.edu>
2026-02-10 22:21:14 -0500

    Some starting to structure of main program

--- JottQL.java
+++ JottQL.java
@@ -1,2 +1,98 @@
+import DDLParser.ParserDDL;
+
+import java.util.Scanner;
+
+/**
+ * Main class for running the entire Project
+ */
 public class JottQL {
+    public static final String QUIT_MESSAGE = "<QUIT>";
+
+    /**
+     * Initialize Database here
+     */
+    private static void startUp(String dbLocation, int pageSize, int bufferSize,
+                                        boolean indexing, boolean debug){
+
+
+    }
+
+    /**
+     * Handle shutdown procedure here
+     */
+    private static void shutdown(){
+
+    }
+
+    /**
+     * The main program and loop
+     * @param args Command-line arguments in the following order:
+     *             dbLocation: directory of database
+     *             pageSize: byte size of page
+     *             bufferSize: number of pages buffer can hold
+     *             indexing: True/False: for indexing
+     *             debug: True/False: enables Logging
+     */
+    public static void main(String[] args) {
+
+        if(args.length >5 || args.length < 4){
+            System.out.println("Usage: java jottQL <dblocation> <pageSize> <bufferSize> <indexing>");
+            return;
+        }
+        System.out.println("Welcome to JottQL!");
+
+        try {
+            String dbLocation = args[0];
+            int pageSize = Integer.parseInt(args[1]);
+            int bufferSize = Integer.parseInt(args[2]);
+            boolean indexing = Boolean.parseBoolean(args[3]);
+            boolean debug = false;
+            if (args.length == 5) {
+                debug = Boolean.parseBoolean(args[4]);
+            }
+
+            startUp(dbLocation, pageSize, bufferSize, indexing, debug);
+            Scanner input = new Scanner(System.in);
+
+            while (true) {
+                boolean commandReady = false;
+                String message = "";
+                while(!commandReady) {
+
+
+                    message += input.nextLine();
+                    if(message.contains(";")){
+                        commandReady = true;
+                        //trim message to end in semicolon
+                        message = message.substring(0, message.indexOf(";") + 1);
+                    }
+                }
+
+                String[] keywords = message.split("\\s+");
+
+                if (message.equals(QUIT_MESSAGE)) {
+                    shutdown();
+                    return;
+
+                } else if (keywords[0].equals("CREATE") ||
+                        keywords[0].equals("ALTER") ||
+                        keywords[0].equals("DROP")){
+                    //DDL parser handles
+                    ParserDDL.parseCommand(message);
+
+                } else if (keywords[0].equals("SELECT")){
+                    //DML parser handles
+                }
+                else{ //does not match any of our cases
+                    System.out.println("Unrecognized command in following input:\n" + message);
+                }
+
+            }
+        }
+        catch(Exception e){
+            //TODO do something here or move inside while true loop so we can loop gracefully
+            }
+
+
+    }
 }
