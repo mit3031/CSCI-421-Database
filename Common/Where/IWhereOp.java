bf0d60b5ed2dc06aacac277b974509583da5f657
Antonio <acb9430@rit.edu>
2026-03-19 10:27:12 -0400

    Create Where directory and create Interface for Where Operands

---
+++ Common/Where/IWhereOp.java
@@ -0,0 +1,21 @@
+package Common.Where;
+
+import java.util.ArrayList;
+
+public interface IWhereOp {
+
+    /**
+     * Gets the "priority level" of the operand, for the Shunting-Yard Algorithm
+     * 1 is highest priority,
+     * @return the priority level
+     */
+    public int getPriority();
+
+    /**
+     * Evaluates a tuple
+     * @return whether or not the tuple satistfies this WhereOp
+     */
+    public boolean evaluate(ArrayList<Object> tuple);
+
+
+}
